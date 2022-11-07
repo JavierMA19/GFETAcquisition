2242ece5a8b76c34322750af911d03ea4b681e81
Anton Guimerà Brunet <anton.guimera@csic.es>
2022-11-07 15:33:03 +0100

    New Configuration Start

---
+++ GFETAcquisition/ParamConf/HardwareConf.py
@@ -0,0 +1,307 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 19 10:56:29 2020
+
+@author: Javier
+"""
+
+# from PyQt5 import Qt
+import pyqtgraph.parametertree.parameterTypes as pTypes
+from PyQt5 import Qt
+
+import GFETCharact.ParamConf.HwConfig as BoardConf
+
+
+class HwGainsConfig(pTypes.GroupParameter):
+    def __init__(self, Gains, **kwargs):
+        super(HwGainsConfig, self).__init__(**kwargs)
+        for n, v in Gains.items():
+            self.addChild({'name': n,
+                           'type': 'float',
+                           'value': v,
+                           'default': v,
+                           'siPrefix': True,
+                           'suffix': ' '}, )
+
+    def GetGains(self):
+        Gains = {}
+        for p in self.children():
+            Gains[p.name()] = p.value()
+        return Gains
+
+
+class AInputsConfig(pTypes.GroupParameter):
+    def __init__(self, AInputs, **kwargs):
+        super(AInputsConfig, self).__init__(**kwargs)
+
+        self.addChild({'name': 'SelAll',
+                       'type': 'action',
+                       'title': 'Un-Select all'})
+        self.addChild({'name': 'SelInvert',
+                       'type': 'action',
+                       'title': 'Invert Selection'})
+        self.seltrufalse = True
+        self.param('SelAll').sigActivated.connect(self.on_SelAll)
+        self.param('SelInvert').sigActivated.connect(self.on_SelInvert)
+
+        for n, v in sorted(AInputs.items()):
+            self.addChild({'name': n,
+                           'type': 'bool',
+                           'value': True,
+                           'aiDC': v[0],
+                           'aiAC': v[-1],
+                           }, )
+
+    def on_SelAll(self):
+        self.seltrufalse = not self.seltrufalse
+        for p in self.children():
+            if p.type() == 'action':
+                continue
+            p.setValue(self.seltrufalse)
+
+    def on_SelInvert(self):
+        for p in self.children():
+            if p.type() == 'action':
+                continue
+            p.setValue(not p.value())
+
+    def GetDCChannels(self):
+        DCChannels = {}
+        for p in self.children():
+            if p.type() == 'action':
+                continue
+            if p.value():
+                DCChannels[p.name()] = p.opts['aiDC']
+        return DCChannels
+
+    def GetACChannels(self):
+        ACChannels = {}
+        for p in self.children():
+            if p.type() == 'action':
+                continue
+            if p.value():
+                ACChannels[p.name()] = p.opts['aiAC']
+        return ACChannels
+
+
+class AOutputsConfig(pTypes.GroupParameter):
+    def __init__(self, AOutputs, **kwargs):
+        super(AOutputsConfig, self).__init__(**kwargs)
+        for n, v in AOutputs.items():
+            self.addChild({'name': n,
+                           'type': 'group',
+                           # 'expanded': False,
+                           'children': [{'name': 'Output',
+                                         'type': 'str',
+                                         'default': v,
+                                         'value': v,
+                                         },
+                                        {'name': 'Selected',
+                                         'type': 'bool',
+                                         'value': True,
+                                         'default': True,
+                                         },
+                                        {'name': 'Value',
+                                         'type': 'float',
+                                         'value': 0,
+                                         'default': 0,
+                                         'step': 0.01,
+                                         'siPrefix': True,
+                                         'suffix': 'V'
+                                         }]
+                           })
+            # self.addChild({'name': n,
+            #                'type': 'str',
+            #                'value': v,
+            #                }, )
+
+    def GetAOuts(self):
+        Aouts = {}
+        for p in self.children():
+            if p.param('Selected').value():
+                Aouts[p.name()] = {'Output': p.param('Output').value(),
+                                   'Value': p.param('Value').value()}
+        return Aouts
+
+
+class ACDCSwitchConfig(pTypes.GroupParameter):
+    def __init__(self, ACDCSwitch, **kwargs):
+        super(ACDCSwitchConfig, self).__init__(**kwargs)
+
+        self.douts = ACDCSwitch['douts']
+        self.addChild({'name': 'douts',
+                       'type': 'str',
+                       'readonly': True,
+                       'value': str(ACDCSwitch['douts']),
+                       })
+        self.GateAI = ACDCSwitch['GateAI']
+        self.addChild({'name': 'GateAI',
+                       'type': 'str',
+                       'readonly': True,
+                       'value': str(ACDCSwitch['GateAI']),
+                       })
+
+        self.States = ACDCSwitch['states']
+        StateChilds = []
+        for n, v in ACDCSwitch['states'].items():
+            StateChilds.append({'name': n,
+                                'type': 'str',
+                                'readonly': True,
+                                'value': str(v),
+                                }, )
+        self.addChild({'name': 'States',
+                       'type': 'group',
+                       'expanded': True,
+                       'children': StateChilds,
+                       })
+
+
+class BoardConfig(pTypes.GroupParameter):
+    def __init__(self, Board, **kwargs):
+        super(BoardConfig, self).__init__(**kwargs)
+
+        self.Gains = HwGainsConfig(Gains=Board['Gains'],
+                                   name='Gains',
+                                   expanded=False, )
+        self.AInputs = AInputsConfig(AInputs=Board['AnalogInputs'],
+                                     name='AInputs',
+                                     title='Analog Inputs',
+                                     expanded=False, )
+        self.AOutputs = AOutputsConfig(AOutputs=Board['AnalogOutputs'],
+                                       name='AOutputs',
+                                       title='Analog Outputs',
+                                       expanded=False, )
+        self.addChildren((self.Gains,
+                          self.AInputs,
+                          self.AOutputs))
+
+        if 'ACDCSwitch' in Board:
+            self.ACDCSwitch = ACDCSwitchConfig(ACDCSwitch=Board['ACDCSwitch'],
+                                               name='ACDCSwitch',
+                                               # title='Analog Outputs',
+                                               expanded=False, )
+            self.addChildren((self.ACDCSwitch,))
+        else:
+            self.ACDCSwitch = None
+
+
+class ColumnsConfig(pTypes.GroupParameter):
+    def __init__(self, Columns, **kwargs):
+        super(ColumnsConfig, self).__init__(**kwargs)
+
+        self.addChild({'name': 'SelAll',
+                       'type': 'action',
+                       'title': 'Un-Select all'})
+        self.addChild({'name': 'SelInvert',
+                       'type': 'action',
+                       'title': 'Invert Selection'})
+        self.seltruefalse = True
+        self.param('SelAll').sigActivated.connect(self.on_SelAll)
+        self.param('SelInvert').sigActivated.connect(self.on_SelInvert)
+
+        for n, v in sorted(Columns.items()):
+            self.addChild({'name': n,
+                           'type': 'bool',
+                           'value': True,
+                           'dout': v,
+                           }, )
+
+    def on_SelAll(self):
+        self.seltruefalse = not self.seltruefalse
+        for p in self.children():
+            if p.type() == 'action':
+                continue
+            p.setValue(self.seltrufalse)
+
+    def on_SelInvert(self):
+        for p in self.children():
+            if p.type() == 'action':
+                continue
+            p.setValue(not p.value())
+
+    def GetColumns(self):
+        Cols = {}
+        for p in self.children():
+            if p.type() == 'action':
+                continue
+            if p.value():
+                Cols[p.name()] = p.opts['dout']
+        return Cols
+
+
+class SwitchMatrixConfig(pTypes.GroupParameter):
+    def __init__(self, Board, **kwargs):
+        super(SwitchMatrixConfig, self).__init__(**kwargs)
+        if Board is None:
+            self.SwitchMatrixPresent = False
+            return
+        self.SwitchMatrixPresent = True
+
+        self.douts = Board['douts']
+        self.addChild({'name': 'douts',
+                       'type': 'str',
+                       'readonly': True,
+                       'value': str(Board['douts']),
+                       })
+
+        self.Columns = ColumnsConfig(Columns=Board['Columns'],
+                                     name='Columns',
+                                     title='Columns Selection',
+                                     expanded=False, )
+
+        self.AOutputs = AOutputsConfig(AOutputs=Board['AnalogOutputs'],
+                                       name='AOutputs',
+                                       title='Analog Outputs',
+                                       expanded=False, )
+
+        self.addChildren((self.AOutputs, self.Columns))
+
+
+class HardwareConfig(pTypes.GroupParameter):
+    on_board_sel = Qt.pyqtSignal(object)
+
+    def __init__(self, **kwargs):
+        super(HardwareConfig, self).__init__(**kwargs)
+        self.addChild({'name': 'BoardSel',
+                       'title': 'Board Selection',
+                       'type': 'list',
+                       'values': BoardConf.Boards.keys(),
+                       })
+
+        self.param('BoardSel').sigValueChanged.connect(self.on_BoardSel)
+        self.Add_Board()
+
+        self.addChild({'name': 'SwitchMatrixSel',
+                       'title': 'Switch Matrix Selection',
+                       'type': 'list',
+                       'values': ['None', ] + list(BoardConf.SwitchMatrix.keys()),
+                       'value': 'None',
+                       })
+        self.param('SwitchMatrixSel').sigValueChanged.connect(self.on_SwitchMatrixSel)
+        self.Add_SwitchSel()
+
+    def on_BoardSel(self):
+        self.param('BoardConf').remove()
+        self.Add_Board()
+        self.on_board_sel.emit(self.param('BoardConf'))
+
+    def Add_Board(self):
+        Board = self.param('BoardSel').value()
+        self.addChild(BoardConfig(Board=BoardConf.Boards[Board],
+                                  name='BoardConf',
+                                  title='Hardware Configuration'))
+
+    def on_SwitchMatrixSel(self):
+        self.param('SwitchMatrixConf').remove()
+        self.Add_SwitchSel()
+
+    def Add_SwitchSel(self):
+        Switch = self.param('SwitchMatrixSel').value()
+        if Switch == 'None':
+            Board = None
+        else:
+            Board = BoardConf.SwitchMatrix[Switch]
+
+        self.addChild(SwitchMatrixConfig(Board=Board,
+                                         name='SwitchMatrixConf',
+                                         title='Switch Matrix Configuration'))
---
+++ GFETAcquisition/ParamConf/HwConfig.py
@@ -0,0 +1,157 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep  8 11:14:17 2020
+
+@author: javi8
+"""
+
+import numpy as np
+
+###
+# Boards definition
+###
+
+DefaultGains = {'DCGain': 10e3,
+                'ACGain': 1e6,
+                'GateGain': 2e6}
+
+MainBoard = {'Gains': DefaultGains,
+             'AnalogInputs': {'Ch01': ('ai0', 'ai8'),
+                              'Ch02': ('ai1', 'ai9'),
+                              'Ch03': ('ai2', 'ai10'),
+                              'Ch04': ('ai3', 'ai11'),
+                              'Ch05': ('ai4', 'ai12'),
+                              'Ch06': ('ai5', 'ai13'),
+                              'Ch07': ('ai6', 'ai14'),
+                              'Ch08': ('ai7', 'ai15'),
+                              'Ch09': ('ai16', 'ai24'),
+                              'Ch10': ('ai17', 'ai25'),
+                              'Ch11': ('ai18', 'ai26'),
+                              'Ch12': ('ai19', 'ai27'),
+                              'Ch13': ('ai20', 'ai28'),
+                              'Ch14': ('ai21', 'ai29'),
+                              'Ch15': ('ai22', 'ai30'),
+                              'Ch16': ('ai23', 'ai31')
+                              },
+             'AnalogOutputs': {'Vs': 'ao1',
+                               'Vds': 'ao0',
+                               'Vg': 'ao2',
+                               },
+             }
+
+MB42 = {'Gains': {'DCGain': 10e3,
+                  'ACGain': 1e6,
+                  },
+        'AnalogInputs': {
+            'Ch01': ('ai16', 'ai24'),
+            'Ch02': ('ai17', 'ai25'),
+            'Ch03': ('ai18', 'ai26'),
+            'Ch04': ('ai19', 'ai27'),
+            'Ch05': ('ai20', 'ai28'),
+            'Ch06': ('ai21', 'ai29'),
+            'Ch07': ('ai22', 'ai30'),
+            'Ch08': ('ai23', 'ai31'),
+            'Ch09': ('ai0', 'ai8'),
+            'Ch10': ('ai1', 'ai9'),
+            'Ch11': ('ai2', 'ai10'),
+            'Ch12': ('ai3', 'ai11'),
+            'Ch13': ('ai4', 'ai12'),
+            'Ch14': ('ai5', 'ai13'),
+            'Ch15': ('ai6', 'ai14'),
+            'Ch16': ('ai7', 'ai15'),
+        },
+        'AnalogOutputs': {'Vs': 'ao1',
+                          'Vds': 'ao0',
+                          },
+        }
+
+MainBoard_v3 = {'Gains': DefaultGains,
+                'AnalogInputs': {'Ch01': ('ai8',),
+                                 'Ch11': ('ai12',),
+                                 'Ch03': ('ai9',),
+                                 'Ch09': ('ai15',),
+                                 'Ch05': ('ai10',),
+                                 'Ch15': ('ai14',),
+                                 'Ch07': ('ai11',),
+                                 'Ch13': ('ai13',),
+                                 'Ch02': ('ai0',),
+                                 'Ch12': ('ai4',),
+                                 'Ch04': ('ai1',),
+                                 'Ch10': ('ai7',),
+                                 'Ch06': ('ai2',),
+                                 'Ch16': ('ai6',),
+                                 'Ch08': ('ai3',),
+                                 'Ch14': ('ai5',),
+                                 'Ch27': ('ai27',),
+                                 'Ch17': ('ai29',),
+                                 'Ch25': ('ai26',),
+                                 'Ch19': ('ai30',),
+                                 'Ch31': ('ai25',),
+                                 'Ch21': ('ai31',),
+                                 'Ch29': ('ai24',),
+                                 'Ch23': ('ai28',),
+                                 'Ch28': ('ai19',),
+                                 'Ch18': ('ai21',),
+                                 'Ch26': ('ai18',),
+                                 'Ch20': ('ai22',),
+                                 'Ch32': ('ai17',),
+                                 'Ch22': ('ai23',),
+                                 'Ch30': ('ai16',),
+                                 'Ch24': ('ai20',)
+                                 },
+                'AnalogOutputs': {'Vs': 'ao1',
+                                  'Vds': 'ao0',
+                                  'Vg': 'ao2',
+                                  },
+                'ACDCSwitch': {'douts': ('port0/line0:8',),
+                               'states': {'DC': np.array([0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+                                                         dtype=np.uint8),
+                                          'AC': np.array([1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                                                         dtype=np.uint8),
+                                          'Gate': np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                                           dtype=np.uint8),
+                                          },
+                               'GateAI': 'ai27',
+                               }
+                }
+
+Boards = {'MainBoard_v3': MainBoard_v3,
+          'MainBoard': MainBoard,
+          'MB42': MB42,
+          }
+
+
+###
+# Switch Matrix definition
+###
+
+def ColumnDecoder(bits=5, nCols=32):
+    Cols = {}
+    sform = '{' + '0:0{:d}b'.format(bits) + '}'
+    for i in range(nCols):
+        name = 'Col{0:02d}'.format(i + 1)
+        Cols[name] = np.array([int(x) for x in list(sform.format(i))], dtype=np.uint8)
+    return Cols
+
+
+Decoder32Cols = {'douts': ('port0/line9:15',),
+                 'Columns': ColumnDecoder(),
+                 'AnalogOutputs': {'Von': 'ao2',
+                                   'Voff': 'ao3',
+                                   },
+                 }
+
+SwitchMatrix = {'Decoder32Cols': Decoder32Cols,
+                }
+
+# if __name__ == '__main__':
+#
+#     nCols = 32
+#
+#     Cols = {}
+#     sform = '{' + '0:0{:d}b'.format(7) + '}'
+#     for i in range(32):
+#         name = 'Col{0:02d}'.format(i + 1)
+#         Cols[name] = np.array([int(x) for x in list(sform.format(i))], dtype=np.uint8)
+#
+#     print(Cols)
