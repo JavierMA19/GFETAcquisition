fa8ef76df047dea423196edeeceaee07ac8c4566
Anton Guimerà Brunet <anton.guimera@csic.es>
2023-03-13 18:09:02 +0100

    first compilation

--- GFETAcquisition/__init__.py
+++ GFETAcquisition/__init__.py
@@ -5,7 +5,10 @@ Created on Tue Nov 15 17:27:23 2016
 @author: aguimera
 """
 
-from _version import __version__
+# from _version import __version__
+
+__version__ = "0.4.5"
 
 __MinBufferTime__ = 0.2
 __DEBUG__ = True
+
